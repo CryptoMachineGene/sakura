6510b5e1ed0173b18122fbe98b3c73ff89e06931
CryptoMachine Gene <27248818+CryptoMachineGene@users.noreply.github.com>
2025-10-24 20:19:55 -0400

    Sakura contract deployed successfully to Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-verify";
 
 const privateKeys = (process.env.PRIVATE_KEYS || "").split(",").filter(Boolean);
 
@@ -13,7 +12,6 @@ const config: HardhatUserConfig = {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY || ""}`,
       accounts: privateKeys
     }
-  },
-  etherscan: { apiKey: process.env.ETHERSCAN_API_KEY || "" }
+  }
 };
 export default config;
